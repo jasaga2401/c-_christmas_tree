9716fdd34f8fc73923541fb28b97ab3a6447dbbe
jasaga2401 <jasaga@protonmail.com>
2023-12-18 10:17:56 +0000

    Add project files.

---
+++ c#_christmas_tree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int treeHeight = 100; // You can change this to get a bigger or smaller tree
+        for (int i = 0; i < treeHeight; i++)
+        {
+            // Print leading spaces
+            for (int j = 0; j < treeHeight - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // Print asterisks
+            for (int j = 0; j < (2 * i + 1); j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        // Print the tree trunk
+        for (int i = 0; i < treeHeight / 2; i++)
+        {
+            for (int j = 0; j < treeHeight - 1; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("*");
+        }
+        Console.ReadLine(); // Keeps the console window open
+    }
+}
+
